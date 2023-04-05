b47bd577e87cd2bb1731478dfeccaac48fccf7b1
alaminsun <sunmoon843@gmail.com>
2023-04-05 12:57:18 +0600

    Third commit

--- ParkeyAPI/Controllers/NationalParksController.cs
+++ ParkeyAPI/Controllers/NationalParksController.cs
@@ -1,5 +1,6 @@
 ﻿using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
+using ParkeyAPI.Models;
 using ParkeyAPI.Models.Dtos;
 using ParkeyAPI.Repository.IRepository;
 using System.Collections.Generic;
@@ -30,5 +31,48 @@ namespace ParkeyAPI.Controllers
             }
             return Ok(objDto);
         }
+
+        [HttpGet("{nationalParkId:int}")]
+        public IActionResult GetNationalPark(int nationalParkId)
+        {
+            var obj = _npRepo.GetNationalPark(nationalParkId);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            var objDto = _mapper.Map<NationalParkDto>(obj);
+            return Ok(objDto);
+        }
+
+        [HttpPost]
+        public IActionResult CreateNationalPark([FromBody] NationalParkDto nationalParkDto)
+        {
+            if (nationalParkDto == null)
+            {
+                return BadRequest(ModelState);
+            }
+            if (_npRepo.NationalParkExists(nationalParkDto.Name))
+            {
+                ModelState.AddModelError("", "National Park Exists!");
+                return StatusCode(404, ModelState);
+            }
+
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            var nationalParkObj = _mapper.Map<NationalPark>(nationalParkDto);
+
+            if (!_npRepo.CreateNatinalPark(nationalParkObj))
+            {
+                ModelState.AddModelError("", $"Something went wrong when save the record {nationalParkObj.Name}");
+                return StatusCode(500, ModelState);
+            }
+
+            return Ok();
+        }
+
+
     }
 }
