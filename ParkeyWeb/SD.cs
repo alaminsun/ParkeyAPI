552a900d88b7871f4cf609a13d2e870f9f2b3b1f
alaminsun <sunmoon843@gmail.com>
2023-04-16 00:39:46 +0600

    nineteen

---
+++ ParkeyWeb/SD.cs
@@ -0,0 +1,9 @@
+﻿namespace ParkeyWeb
+{
+    public static class SD
+    {
+        public static string APIBaseUrl = "https://localhost:44341/";
+        public static string NationalParkAPIPath = APIBaseUrl+ "api/v1/nationalparks";
+        public static string TrailAPIPath = APIBaseUrl+ "api/v1/trails";
+    }
+}
